fb9d552bf3320fd8f9691fe207f78ff15be87c9f
Liang DiYuan <1104152218@qq.com>
2020-09-05 07:41:38 +0800

    完成登录

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,7 +1,7 @@
-const apiUrl = "http://127.0.0.1:7001";
+const ipUrl = "http://127.0.0.1:7001";
 
 const servicePath = {
-  login: `${apiUrl}/admin/login`,
+  login: `${ipUrl}/admin/login`,
 };
 
 export default servicePath;
